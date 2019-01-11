88f40dafa3af5bbdedfd469bdeda15d122f237e9
HAMISH <constatcopywriting@gmail.com>
2019-01-11 15:24:53 +1300

    Added input, which refouses on mount/update, and with every user interaction with the page. This input will be the primary object of user interactions.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ProgressBar from './ProgressBar'
 import { createEventHandler } from 'recompose'
+import posed from 'react-pose'
+
+const BarContainer = posed.div({
+  hoverable: true,
+  init: {
+      scale: 1,
+      boxShadow: '0px 0px 0px rgba(0,0,0,0)'
+  },
+  hover: {
+      scale: 1.02,
+      boxShadow: '0px 5px 10px rgba(0,0,0,0.2)'
+  }
+})
 
 const App = () => {
   const { stream: click$, handler: eventHandler } = createEventHandler()
+  const ref = React.createRef()
+  const focusOnInput = () => ref.current.focus()
+
+  useEffect(focusOnInput)
 
   return (
-    <div className="App" style={{ display: 'flex' }}>
-      <div style={{ margin: 20 }}>
+    <div className="App" style={{ display: 'flex' }} onClick={focusOnInput}>
+      <input 
+        type="text"
+        ref={ref} 
+        onKeyPress={eventHandler}
+        style={{ height: 0 }} 
+      />
+      <BarContainer style={{ margin: 20, borderRadius: 15 }}>
         <ProgressBar 
           level={100} 
           height={400}
@@ -25,25 +48,19 @@ const App = () => {
           eventStream={click$}
           eventHandler={eventHandler}
         />
-      </div>
+      </BarContainer>
       <div style={{ margin: 20 }}>
         <ProgressBar 
-          level={100} 
-          height={400}
-          width={30} 
-          margin={5}
+          level={100}
+          width={60}
+          height={400} 
           complete={() => alert('fuck')}
           empty={() => {}}
           transitionDuration={100}
           incrementValue={50} 
           decrementValue={10}
-          border="1px solid grey"
-          borderRadius={15}
-          color="linear-gradient(blue, purple)"
-          onClickEvent={true}
         />
       </div>
-      <button onClick={eventHandler}>click</button>
     </div>
   )
 }
--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -2,22 +2,9 @@ import React from 'react'
 import { componentFromStream, createEventHandler } from 'recompose'
 import { map, startWith, switchMap, mapTo, scan } from 'rxjs/operators'
 import { merge, interval } from 'rxjs'
-import posed from 'react-pose'
 import './observableConfig'
 
-const OuterBox = posed.div({
-    hoverable: true,
-    init: {
-        scale: 1,
-        boxShadow: '0px 0px 0px rgba(0,0,0,0)'
-    },
-    hover: {
-        scale: 1.02,
-        boxShadow: '0px 5px 10px rgba(0,0,0,0.2)'
-    }
-})
-
-function BarPresentational(props) {
+const BarPresentational = props => {
     const { 
         level, 
         eventHandler,
@@ -43,24 +30,24 @@ function BarPresentational(props) {
         },
         inner: {
             background: color,
-            width: level > (borderRadius * 1) ? width : width - (borderRadius),
+            width: level > (borderRadius * 1) ? width : width - borderRadius,
             height: level,
             margin: margin,
             borderRadius: borderRadius - margin,
-            opacity: level > (borderRadius * 1) ? 1 : 0,
+            opacity: level > borderRadius ? 1 : 0,
             transitionProperty: 'height width opacity',
             transitionDuration: transitionDuration / 1000 + 's',
             transitionTimingFunction: 'linear'
         }
     }
-
+    
     return(
-        <OuterBox 
+        <div 
             style={styles.container} 
             onClick={onClickEvent ? eventHandler : () => {}}
         >
             <div style={styles.inner}></div>
-        </OuterBox>
+        </div>
     )
 }
 
@@ -97,16 +84,16 @@ const ProgressBar = componentFromStream(prop$ => (
                 }),
                 map(level => ({ 
                     level,
-                    color: props.color || 'coral',
+                    color: props.color || "linear-gradient(blue, purple)",
                     eventHandler,
                     eventStream: click$,
                     onClickEvent: props.onClickEvent || true,
-                    margin: props.margin, 
+                    margin: props.margin || 5, 
                     height: props.height,
-                    width: props.width,
-                    borderRadius: props.borderRadius || props.width / 20,
-                    border: props.border || "1px solid black",
-                    transitionDuration: props.transitionDuration,
+                    width: props.width || 30,
+                    borderRadius: props.borderRadius || (props.width ? props.width / 2 : 15),
+                    border: props.border || "1px solid grey",
+                    transitionDuration: props.transitionDuration
                 })),
                 map(BarPresentational)
             )
